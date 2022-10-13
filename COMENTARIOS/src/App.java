99f8e449b683aae1a993c3355eb4b8ee35f772a7
Francisco Navarro <fran27_navarro@hotmail.com>
2022-10-13 17:46:30 -0300

    first commit java_practices

---
+++ COMENTARIOS/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * En esta practica dejo plasmado como se hacen los COMENTARIOS en Java
+         * -es igual a los comentarios en JavaScript:
+         */
+
+
+        // esto es un comentario de una sola linea
+
+        /*
+         * esto es un comentario de multiples lineas
+         */
+    }
+}
