c2c967d22d00528e6df1fc23f4107542b7a2cc1b
Francisco Navarro <fran27_navarro@hotmail.com>
2022-10-16 17:42:58 -0300

    variables en java

---
+++ capitulo2/src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables{
+    public static void main(String[] args) throws Exception {
+        
+        // estas son las formas de declarar y reasignar variables en java :)
+
+        int variable1;
+        int variable2, variable3;
+        int variable4= 23;
+
+        variable1= 6;
+
+        System.out.println("variable 1 contiene el valor: "+variable1);
+
+        variable2= 2 * variable1;
+
+
+
+
+    }
+}
