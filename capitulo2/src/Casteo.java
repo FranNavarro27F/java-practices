36a7d3a76d7b76338e673283d5a58405f0615a2e
Francisco Navarro <fran27_navarro@hotmail.com>
2022-10-17 11:38:26 -0300

    casteo de datos en java

--- capitulo2/src/Casteo.java
+++ capitulo2/src/Casteo.java
@@ -1,5 +1,31 @@
 public class Casteo {
     public static void main(String[] args) {
         
+        // em este ejemplo generamos un casteo de tipo int a float
+        int variableDeTipoInt= 10;
+        float variableDeTipoFloat= variableDeTipoInt;
+        System.out.println("la variable de tipo float vale: "+ variableDeTipoFloat);
+
+        // aca hacemos casteo de tipo float a double
+        double variableDeTipoDouble= variableDeTipoFloat;
+        System.out.println("la variable de tipo double vale: "+variableDeTipoDouble);
+
+        // otra forma de casteo 
+        double otraVariableDeTipoDouble= 2.76;
+        int otraVariableDeTipoInt= (int)otraVariableDeTipoDouble;
+        System.out.println("el resultado del casteo de otraVariableDeTipoDouble a int es: "+ otraVariableDeTipoInt);
+
+        byte variable1 = 5;
+        byte variable2 = 10;
+        byte suma= (byte)(variable1 + variable2);
+        System.out.println("el resultado del casteo a byte es: "+ suma);
+        
+
+        
+
+
+
+
+
     }
 }
